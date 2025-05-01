7089d5d10771cfaa96a273d54a5a093173856f9a
Nicolas <nicolashenrique921@gmail.com>
2025-05-01 18:42:59 -0300

    correção

--- app/index.js
+++ app/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const escape = require('escape-html');
+const helmet = require('helmet');
+
 const app = express();
 const port = 3000;
 
+// Middleware de segurança para headers HTTP
+app.use(helmet());
+
 app.get('/hello', (req, res) => {
-  const name = req.query.name;
-  res.send(`Olá ${name}`); // vulnerável a XSS
+  // Escapando a entrada do usuário para evitar XSS
+  const name = escape(req.query.name || 'visitante');
+  res.send(`Olá ${name}`);
 });
 
 app.listen(port, () => {
